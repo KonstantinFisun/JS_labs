b7631d02dd35ef96292d1d476563bb237153dae4
Константин Сергеевич Фисун <kostya.ficyn@gmail.com>
2022-05-23 22:22:35 +0300

    autorization

---
+++ PHP/autorization/logout.php
@@ -0,0 +1,21 @@
+<?php
+   if (!empty($_COOKIE)) {
+    require __DIR__ . '/auth.php';
+    if (isset($_COOKIE['login'])) {
+        $login = $_COOKIE['login'];
+    } else {
+        // присваиваем $login значение '' если $_COOKIE['login'] равен NULL
+        $login = '';
+    }
+    if (isset($_COOKIE['password'])) {
+        $password = $_COOKIE['password'];
+    } else {
+        // присваиваем $password значение '' если $_COOKIE['password'] равен NULL
+        $password = '';
+    }
+
+    $password = $_COOKIE['password'];
+    if (checkAuth($login, $password)) {
+        header('Location: /login.php');
+    }
+}
